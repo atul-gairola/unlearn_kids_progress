6722f9709a27432b2161e09ea1b5eedece2f98b5
Atul Gairola <atulgairola08@gmail.com>
2020-05-26 19:43:21 +0530

    Complete progress site - final commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 $(function () {
-  $('[data-toggle="popover"]').popover({
-    trigger: "click"
-  })
+  $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+const status = Array.from(document.querySelectorAll('.status'));
+status.forEach(cur => {
+  if (cur.innerHTML === "Published")
+  cur.style.color = '#0ACF83';
+  else
+  cur.style.color = '#EB5757';
+});
